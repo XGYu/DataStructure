0daaee702881152c619bf039e90c4c28eda1d61e
XGYu <1643513827@qq.com>
2020-08-06 20:44:46 +0800

    图的迭代器

--- ds/Graph/DenseGraph.h
+++ ds/Graph/DenseGraph.h
@@ -39,7 +39,6 @@ public:
     void addEdge(int v, int w) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
-        assert(v != w);
         if(hasEdge(v, w))   return;
 
         g[v][w] = true;
@@ -55,6 +54,48 @@ public:
 
         return g[v][w];
     }
+
+    //TODO 邻边迭代器，传入一个图和一个顶点
+    class adjIterator {
+    private:
+        DenseGraph &G;
+        int v;
+        int index;
+
+    public:
+        adjIterator(DenseGraph &graph, int v): G(graph) {
+            assert(v >= 0 && v < G.n);
+            this->v = v;
+            this->index = -1;
+        }
+
+        ~adjIterator() {
+
+        }
+
+        //返回图中与顶点v相连的第一个顶点
+        int begin() {
+            index = -1; //由于要使用next()，所以index从-1开始
+            return next();
+        }
+
+        //返回与顶点v相连的下一个顶点
+        int next() {
+            for(index += 1; index < G.V(); index ++)
+                if(G.g[v][index])
+                    return index;
+
+            return -1;
+        }
+
+        //查看是否已经迭代完成了与v相连接的所有顶点
+        bool end() {
+            return index >= G.V();
+        }
+    };
+
 };
+
+
 
 #endif //GRAPH_DENSEGRAPH_H
